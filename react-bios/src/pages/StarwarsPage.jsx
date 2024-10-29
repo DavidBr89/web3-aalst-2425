93ad8356c40296d5ce54d679e66b4fc0bdd1064d
David Breckx <david.breckx@telenet.be>
2024-10-29 12:17:15 +0100

    Einde les 6

--- react-bios/src/pages/StarwarsPage.jsx
+++ react-bios/src/pages/StarwarsPage.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const StarwarsPage = () => {
-  return <div>StarwarsPage</div>;
+  const [swMovies, setSwMovies] = useState();
+
+  // OPTIE 1: fetch API
+
+  const fetchSwMovies = async () => {
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+      const data = await response.json();
+
+      setSwMovies(data);
+
+      console.log("SWAPI:", data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    // Dit is een side effect -> code dat React zelf niet in de hand heeft
+    fetchSwMovies();
+  }, []);
+
+  return (
+    <div>
+      {swMovies.results.map((m) => (
+        <p key={m.episode_id}>{m.title}</p>
+      ))}
+    </div>
+  );
 };
 
 export default StarwarsPage;
